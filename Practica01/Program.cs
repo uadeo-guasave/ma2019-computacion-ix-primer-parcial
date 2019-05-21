44865d46eb5c3abc9ccce8c4920b72ea9dd95495
Bidkar Aragón Cárdenas <bidkar.aragon@udo.mx>
2019-05-21 16:42:53 -0600

    Lectura de Directorios y Archivos

--- Practica01/Program.cs
+++ Practica01/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.IO;
 
 namespace Practica01
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // Directorios();
+      // Archivos();
+
+      Console.ReadLine();
     }
+
+    private static void Archivos()
+    {
+      string ruta = @"/Users/bidkar/Documents/UDO/2019/Mayo-Agosto/Computacion9/PrimerParcial/Datos";
+
+      var archivos = Directory.GetFiles(ruta);
+      foreach (var a in archivos)
+      {
+        Console.WriteLine($"Archivo: {Path.GetFileName(a)}");
+        var contenido = File.ReadAllLines(a);
+        foreach (var linea in contenido)
+        {
+          Console.WriteLine(linea);
+        }
+      }
+    }
+
+    private static void Directorios()
+    {
+      string rutaActual = Directory.GetCurrentDirectory();
+      Console.WriteLine($"Directorio actual: {rutaActual}");
+
+      string[] directorios = Directory.GetDirectories(rutaActual, "*", SearchOption.AllDirectories);
+      foreach (var d in directorios)
+      {
+        Console.WriteLine($"Directorio: {d}");
+      }
+    }
+  }
 }
